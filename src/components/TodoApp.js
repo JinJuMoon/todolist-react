90b3f4146ffa6fd2d1a4ab46a2467e0c8642e556
moon <mjj920930@gmail.com>
2020-06-25 00:44:29 +0900

    feat: TodoApp의 상태 중 todos 배열의 원소에 id 추가

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -5,24 +5,27 @@ import TodoCount from "./TodoCount";
 
 const TodoApp = () => {
     const [todos, setTodos] = useState([
-        {title: '새로운 타이틀', state: 'todo'},
-        {title: '완료된 타이틀', state: 'editing'},
-        {title: '완료된 타이틀', state: 'completed'}
+        {id: 0, title: '새로운 타이틀', state: 'todo'},
+        {id: 1, title: '완료된 타이틀', state: 'editing'},
+        {id: 2, title: '완료된 타이틀', state: 'completed'}
     ]);
+
+    const [nextId, setNextId] = useState(3);
 
     const onSubmitTodos = todo => {
         const nextTodos = [
             ...todos,
-            {title: todo.title, state: todo.state}
+            {id: nextId, title: todo.title, state: todo.state}
         ];
         setTodos(nextTodos);
+        setNextId(nextId + 1);
     }
 
     return (
         <section className="todoapp">
             <h1>TODOS</h1>
-            <TodoInput onSubmit={onSubmitTodos}/>
-            <TodoList todos={todos}/>
+            <TodoInput onSubmit={onSubmitTodos} />
+            <TodoList todos={todos} />
             <TodoCount />
         </section>
     );
