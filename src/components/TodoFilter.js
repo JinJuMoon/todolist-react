923c67cabf460fef38fb53a74db779466840a754
moon <mjj920930@gmail.com>
2020-06-25 17:45:47 +0900

    refactor: 불필요한 변수 제거

--- src/components/TodoFilter.js
+++ src/components/TodoFilter.js
@@ -2,12 +2,6 @@ import TodoList from "./TodoList";
 import React from "react";
 
 const TodoFilter = props => {
-    const onChangeItemState = (id, checked) => props.onChangeItemState(id, checked)
-    const onDeleteItem = id => props.onDeleteItem(id)
-    const onStartEditingMode = id => props.onStartEditingMode(id)
-    const onExitEditingMode = id => props.onExitEditingMode(id)
-    const onChangeItemTitle = (id, title) => props.onChangeItemTitle(id, title)
-
     const filterTodos = () => {
         return props.todos.filter(todo => todo.state === props.viewMode);
     }
@@ -17,11 +11,11 @@ const TodoFilter = props => {
     return (
         <TodoList
             todos={filteredTodos}
-            onChangeItemState={onChangeItemState}
-            onDeleteItem={onDeleteItem}
-            onStartEditingMode={onStartEditingMode}
-            onExitEditingMode={onExitEditingMode}
-            onChangeItemTitle={onChangeItemTitle}
+            onChangeItemState={props.onChangeItemState}
+            onDeleteItem={props.onDeleteItem}
+            onStartEditingMode={props.onStartEditingMode}
+            onExitEditingMode={props.onExitEditingMode}
+            onChangeItemTitle={props.onChangeItemTitle}
         />
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,22 +2,16 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = props => {
-    const onChangeItemState = (id, checked) => props.onChangeItemState(id, checked)
-    const onDeleteItem = id => props.onDeleteItem(id)
-    const onStartEditingMode = id => props.onStartEditingMode(id)
-    const onExitEditingMode = id => props.onExitEditingMode(id)
-    const onChangeItemTitle = (id, title) => props.onChangeItemTitle(id, title)
-
     const todosList = props.todos.map(todo => {
         return (
             <TodoItem
                 key={todo.id}
                 todo={todo}
-                onChangeItemState={onChangeItemState}
-                onDeleteItem={onDeleteItem}
-                onStartEditingMode={onStartEditingMode}
-                onExitEditingMode={onExitEditingMode}
-                onChangeItemTitle={onChangeItemTitle}
+                onChangeItemState={props.onChangeItemState}
+                onDeleteItem={props.onDeleteItem}
+                onStartEditingMode={props.onStartEditingMode}
+                onExitEditingMode={props.onExitEditingMode}
+                onChangeItemTitle={props.onChangeItemTitle}
             />
         );
     });
